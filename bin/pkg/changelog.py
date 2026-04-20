d1f28075ebf15409393a243999f94d12cad6ddc3
Cyril Galibern <cyril.galibern@opensvc.com>
2026-04-20 10:51:43 +0200

    Replace `LooseVersion` with `Semver`.as_semver for semantic version sorting in `changelog.py`

--- bin/pkg/changelog.py
+++ bin/pkg/changelog.py
@@ -2,14 +2,15 @@
 
 from __future__ import print_function
 import argparse
-import sys
 from subprocess import *
-from distutils.version import LooseVersion
+import opensvc
+from utilities.semver import Semver
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--commits", type=str, default="HEAD", help="commits boundary. ex : 2.0..HEAD ")
 parser.add_argument("-v", "--verbose", action="store_true", help="add long commit id")
 args = parser.parse_args()
+
 
 def get_commits():
     cmd = ["git", "log", '--pretty=format:%h %H %s', args.commits]
@@ -28,13 +29,20 @@ def get_versions(cids):
     versions = list(out.decode().splitlines())
     return versions
 
+def as_semver(v):
+    # 2.1-1881-g078e7d5f8
+    # ->
+    # 2.1.1881-g078e7d5f8
+    s = v.replace("-", ".", 1)
+    return Semver.parse(s)
+
 def main():
     commits = get_commits()
     cids = [c for c in commits]
     versions = get_versions(cids)
     for i, cid in enumerate(cids):
         commits[cid].insert(0, versions[i])
-    for commit in sorted(commits.values(), key=lambda x: LooseVersion(x[0]), reverse=True):
+    for commit in sorted(commits.values(), key=lambda x: as_semver(x[0]), reverse=True):
         if args.verbose:
             print("%-18s %s  %s" % (commit[0], commit[2], commit[3]))
         else:
