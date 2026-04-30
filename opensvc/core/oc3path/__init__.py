45807e6c7fc0ee3191421e77f7b614d5931856a4
Cyril Galibern <cyril.galibern@opensvc.com>
2026-04-30 17:35:07 +0200

    [node] Add support for OC3 action queue handling with `dequeue_actions_oc3`

--- opensvc/core/oc3path/__init__.py
+++ opensvc/core/oc3path/__init__.py
@@ -3,6 +3,10 @@
 # Server api paths
 SERVER_NODE_REGISTER = "/api/auth/node"
 
+FEED_NODE_ACTIONQ = "/api/node/actionq"
+FEED_NODE_ACTIONQ_DONE = "/api/node/actionq/done"
+FEED_NODE_ACTIONQ_RUNNING = "/api/node/actionq/running"
+
 # Feeder api paths
 FEED_DAEMON_PING = "/api/daemon/ping"
 FEED_DAEMON_STATUS = "/api/daemon/status"
